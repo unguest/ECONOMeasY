626e47d09b011384cf7e9b0769e5cde9c3a5d519
Endeavxor <82844993+Endeavxor@users.noreply.github.com>
2022-09-10 11:58:41 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,123 +1,3 @@
-// ADD QUESTIONS / ANSWER HERE :
-let answers = {
-    "L'offre est la quantité que les producteurs ont l'intention de proposer au marché en fonction des prix possibles" : ["Vrai"],
-    "La pénurie est un excès d'offre sur la demande" : ["Faux"],
-    "La date de l\"économie moderne c’est l’an 2000" : ["Faux"],
-    "« Comprendre l’économie est indispensable pour comprendre la marche du monde » est une affirmation de Vincent Bernard" : ["Vrai"],
-    "L’économie est une science sociale et humaine" : ["Vrai"],
-    "L’opinion publique admet l’existence de 4 révolutions industrielles" : ["Vrai"],
-    "L’économie porte sur l’étude des ressources à disposition des êtres humains" : ["Faux"],
-    "L’économie n’est mondiale que depuis 1800" : ["Faux"],
-    "Le droit de propriété est à la base du raisonnement économique" : ["Vrai"],
-    "L’économétrie est une branche de l’économie" : ["Vrai"],
-    "L’eau de mer est une ressource rare" : ["Faux"],
-    "L’économie distingue plusieurs types de besoins" : ["Vrai"],
-    "L’eau est une ressource rare au sens économique" : ["Faux"],
-    "Le pain est une ressource rare" : ["Vrai"],
-    "L’économie s’occupe aussi des services" : ["Vrai"],
-    "La notion de richesse est similaire à celle de production pour l’économiste" : ["Vrai"],
-    "Le moyen de satisfaire les besoins est la production" : ["Vrai"],
-    "Un bijou avec un diamant est une ressource rare" : ["Vrai"],
-    "Le problème économique vient de la rareté des biens de consommation" : ["Faux"],
-    "La définition économique d’une ressource rare c’est « nécessite un effort »" : ["Vrai"],
-    "Les ressources rares sont des ressources peu présentes sur terre" : ["Faux"],
-    "Les ressources rares peuvent se résumer en capital (au sens productif) et travail" : ["Vrai"],
-    "Une voiture électrique est une ressource rare" : ["Vrai"],
-    "L’économie ne s’occupe que des biens de consommation" : ["Faux"],
-    "L’économie étudie la satisfaction des besoins" : ["Faux"],
-    "Les activités principales ou opérationnelles contribuent directement au processus de production ou commercialisation" : ["Vrai"],
-    "Les activités support ne peuvent pas être créatrices de valeur, elles sont imposées par l'administration de l'entreprise" : ["Faux"],
-    "L'avantage concurrentiel permet à l'entreprise d'accaparer davantage de valeur sur son marché" : ["Vrai"],
-    "La fragmentation des chaînes de valeur signifie que les opérations nécessaires à l'élaboration d'un produit sont éclatées dans un grand nombre de pays" : ["Vrai"],
-    "Un coût est un ensemble de prix" : ["Vrai"],
-    "Un prix est une valeur fixée par le marché" : ["Vrai"],
-    "La notion de valeur en économie est l'importance accordée à un bien ou service" : ["Vrai"],
-    "La mesure du commerce en valeur ajoutée peut donner une idée plus juste de la réelle richesse produite au niveau mondial" : ["Vrai"],
-    "La notion de valeur en économie est l'importance accordée à un bien ou service" : ["Vrai"],
-    "La valeur ajoutée résulte de la soustraction des consommations intermédiaires à la production" : ["Vrai"],
-    "Face à une chaîne de valeur fragmentée, une nouvelle source de valeur tient à la coordination des différents acteurs" : ["Vrai"],
-    "Les marginalistes ont théorisé l'utilité pour expliquer la valeur des biens" : ["Vrai"],
-    "La fragmentation des chaînes de valeur conduit à enregistrer \"comptablement\" plusieurs fois les mêmes opérations pour un même produit" : ["Vrai"],
-    "Dans le cas d'une fragmentation de la production, la valeur totale d'un produit est la somme des valeurs ajoutées se rapportant à ce produit dans chaque pays contribuant à son élaboration" : ["Vrai"],
-    "Coût et prix, c'est la même chose" : ["Faux"],
-    "Les chiffres du commerce extérieur de chaque nation enregistrent d'une part les importations et d'autre part les exportations" : ["Vrai"],
-    "L'utilité est l'aptitude d'un bien ou service à satisfaire un besoin" : ["Vrai"],
-    "La valeur ajoutée est d'autant plus grande que le coût des facteurs de production est faible" : ["Faux"],
-    "La fragmentation des chaînes de valeur conduit à gongler artificiellement le montant des échanges internationaux" : ["Vrai"],
-    "Lorque la demande d'un bien ne varie pas et que l'offre augmente, alors le prix augmente" : ["Faux"],
-    "La variation du prix sur un marché est fonction de l'offre ou de la demande (pas les 2)" : ["Faux"],
-    "L'atomicité du marché signifie qu'il existe un nombre important d'offreurs" : ["Vrai"],
-    "La demande est la quantité que les producteurs ont l'intention de proposer au marché en fonction des prix possibles" : ["Faux"],
-    "Le monopole rend captifs les demandeurs" : ["Vrai"],
-    "La concurrence est la compétition entre entreprises pour proposer leurs produits aux demandeurs" : ["Vrai"],
-    "La concurrence parfaite implique des conditions parfaitement identiques pour tous sur le marché" : ["Vrai"],
-    "Plus le prix est bas, plus il y a de demandeurs" : ["Vrai"],
-    "Les entreprises est l'agent économique dont l'intérêt à agir est le profit" : ["Vrai"],
-    "La loi de l'offre et de la demande explique que la quantité de biens ou services disponibles sur un marché \"libre\" s'ajuste automatiquement à la quantité souhaitée en fonction du prix" : ["Vrai"],
-    "Les producteurs déterminent l'offre" : ["Vrai"],
-    "Un oligopsone est un marché qui présente une multitude d'offreurs et quelques demandeurs" : ["Vrai"],
-    "L'économie d'échelle c'est quand le coût unitaire du produit diminue lorsqu'on produit beaucoup" : ["Vrai"],
-    "La propriété intellectuelle est une restriction à la libre concurrence" : ["Vrai"],
-    "Il existe plusieurs \"marchés\"" : ["Vrai"],
-    "Les acheteurs constituent la demande" : ["Vrai"],
-    "La surproduction est un excès d'offre sur la demande" : ["Vrai"],
-    "L'intérêt des demandeurs est le profit" : ["Faux"],
-    "L'augmentation du coût de production peut être compensée par des gains de productivité" : ["Vrai"],
-    "La loi de l'offre et de la demande explique la formation des prix" : ["Vrai"],
-    "Une institution est un ensemble de règles" : ["Vrai"],
-    "Les vendeurs constituent la demande" : ["Faux"],
-    "L'atomicité du marché signifie qu'il existe un nombre important d'offreurs" : ["Vrai"],
-    "Un oligopole présente le risque d'une entente sur les prix" : ["Vrai"],
-    "L'offre est la quantité que les producteurs ont l'intention de proposer au marché en fonction des prix possibles" : ["Vrai"],
-    "Tout agent économique n'agit que s'il y trouve un intérêt est un postulat de base" : ["Vrai"],
-    "Le corps humain est un bien marchand" : ["Faux"],
-    "La demande peut être stimulée par le marketing" : ["Vrai"],
-    "Lorsque les revenus d'un pays diminuent, l'offre va elle aussi diminuer" : ["Vrai"],
-    "Plus le coût de production est élevé, plus l'offre est élevée" : ["Faux"],
-    "Il existe un marché lorsque les biens sont considérés comme des marchandises" : ["Vrai"],
-    "Un système économique se définit comme étant les \"règles du jeu\" de l'économie (production, répartition et échange)" : ["Vrai"],
-    "En théorie, pour qu'il y ait marché, il faut que les rapports entre offre et demande soient libres" : ["Vrai"],
-    "Sur un marché, agissent des vendeurs et des acheteurs" : ["Vrai"],
-    "La paradoxe : \"la diminution du nombre de producteurs peut faire baisser les prix\" est possible" : ["Vrai"],
-    "L'approche de la loi de l'offre et de la demande est théorique" : ["Vrai"],
-    "En France, le sang n'est pas un bien marchand" : ["Vrai"],
-    "La pénurie est un excès d'offre sur la demande" : ["Faux"],
-    "Selon les monétaristes, l'Etat doit intervenir pour réduire la distribution de revenus" : ["Faux"],
-    "Le courant des monétaristes voit le jour dans un contexte inflationniste" : ["Vrai"],
-    "C'est en soutenant l'investissement que l'Etat peut faire bénéficier l'économie du multiplicateur keynesien" : ["Vrai"],
-    "L'avantage comparatif consiste à se spécialiser là où l'on \"perd\" le moins" : ["Vrai"],
-    "Selon Keynes, l'Etat doit soutenir impérativement la consommation avant toute chose" : ["Faux"],
-    "Marx détermine la cause des crises à partir de la surproduction" : ["Vrai"],
-    "L'avantage absolu consiste à se spécialiser là où on possède la meilleure compétence" : ["Vrai"],
-    "Le coût d’opportunité d’une action ou d’un choix économique est la mesure de la valeur des actions ou choix auxquels on renonce" : ["Vrai"],
-    "Les néo-classiques élaborent une théorie de la valeur subjective" : ["Vrai"],
-    "Les \"classiques\" n'ont pas de considération spécifique pour la monnaie : c'est juste une unité de compte" : ["Vrai"],
-    "Le libéralisme provient de nos égoismes individuels" : ["Vrai"],
-    "La division du travail est indissociable de la liberté des échanges, sinon, ça ne marche pas" : ["Vrai"],
-    "Les physiocrates entérinent le fait que la richesse ne peut venir que de la terre" : ["Vrai"],
-    "Les théories classique et marxiste ont comme point commun que la seule valeur économique qui vaille provient du travail" : ["Vrai"],
-    "La France a pu dévaluer sa monnaie en 2020 (COVID)" : ["Faux"],
-    "Keynes prône une intervention de l'Etat dans l'économie pour résoudre les crises" : ["Vrai"],
-    "La main invisible peut être définie comme la somme de nos intérêts individuels" : ["Vrai"],
-    "La compétence incite à la division du travail ; cette spécialisation accroit les compétences" : ["Vrai"],
-    "C'est l'étude du comportement du consommateur et de la masse monétaire qui est à la base des théories de Friedman" : ["Vrai"],
-    "Que comprend le coût de production ?" : ["Le prix des matières premières","Les salaires","L'amortissement des machines","La note d'EDF"],
-    "Pour qu'il y ait ECONOMIE de marché, quelles conditions doivent être remplies ?" : ["Le droit à la propriété (marché de la terre)", "Un marché de la monnaie et/ou du financement", "Un marché du travail", "Un ou plusieurs marchés de biens et services"],
-    "Quels arguments développe Kaplan pour expliquer la valeur des choses ?" : ["La subjectivité de nos perceptions","Le bénéfice mutuel de l'échange"],
-    "Comment les entreprises peuvent elles améliorer les maillons de leur chaîne de valeur ?" : ["Augmenter la qualification des salariés","Sous-traiter certaines de leurs activités","Développer des relations de coopération avec leurs fournisseurs"],
-    "A quelle grande catégorie de la chaîne de valeur appartient ce maillon ?\nD'après Porter" : ["Gestion des ressources humaines → Fonctionnel ou support","approvisionnement → Opérationnel ou principal","vente → Opérationnel ou principal"],
-    "L'économie s'intéresse à (au-x)..." : ["La production","La distribution", "La consommation", "Les échanges"],
-    "Pourquoi nos besoins sont-ils illimités ?\nCar..." : ["La pauvreté existe très largement sur la planète", "La publicité nous incite à toujours consommer davantage", "Il y a un décalage dans le temps entre le désir et la satisfaction du besoin"],
-    "Le problème économique est que..." : ["les ressources sont rares face à des besoins humains illimités"],
-    "Les facteurs de production sont" : ["Matières premières", "Energie", "Travail", "Capital technique (machines)", "financement"],
-    "A quoi sert la valeur ajoutée ?" : ["A rémunérer les facteurs de production"],
-    "Comment l'entreprise peut-elle capter davantage de valeur sur son marché ?" : ["Produire moins cher que ses concurrents", "Produire mieux pour faire accepter un prix supérieur"],
-    "D'après le schéma de la \"chaîne de valeur\" de Mickael Porter, quelles sont les 2 grandes catégories d'activité dans l'entreprise ?\n... les activités" : ["principales", "opérationnelles", "support", "fonctionnelles"],
-    "La concurrence parfaite implique que le prix du marché s'établit toujours au plus bas" : ["Vrai"],
-    "Quels sont les déterminants de la demande ?" : ["Les revenus", "Les désirs des acheteurs", "Le nombre d'acheteurs"],
-}
-
-
 // Thanks to https://github.com/ka-weihe/fast-dice-coefficient/blob/master/dice.js
 function checkStrSimilarity(fst, snd) {
     var i, j, k, map, match, ref, ref1, sub;
@@ -144,7 +24,6 @@ function checkStrSimilarity(fst, snd) {
     return 2.0 * match / (fst.length + snd.length - 2);
 };
 
-
 // All works here :
 let questions = document.getElementsByClassName("qtext");
 for(question of questions){
@@ -153,7 +32,7 @@ for(question of questions){
     p.style.backgroundColor = "rgb(255, 255, 128)";
     let maxSimilarity = 0;
     let savedQuestion = ""
-    for (const [quest, answ] of Object.entries(answers)) {
+    for (const [quest, answ] of Object.entries(questions_answers)) {
         let similarity = checkStrSimilarity(question.textContent,quest);
         if(similarity > maxSimilarity){
             maxSimilarity = similarity;
@@ -164,7 +43,7 @@ for(question of questions){
         p.textContent = "Not Found";
     }else{
         p.textContent = "Correspondance à " + maxSimilarity*100 + " %  ~" + savedQuestion + "~ :\n";
-        for(answer of answers[savedQuestion]){
+        for(answer of questions_answers[savedQuestion]){
             p.textContent += "• " + answer + "\n";
         }
     }
